b1acf2a58044876d50147cf3a1c86984ad458c92
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-21 16:09:38 +0200

    tags.js correction

--- back/routes/quizz.js
+++ back/routes/quizz.js
@@ -8,12 +8,26 @@ router
         async (req, res) => {
             const result = await pool.query('SELECT * FROM quizz');
             res.json(result.rows);
+            res.status(200).end();
         })
     
+    .get('/:id_creator', async (req,res) => {
+        const result = pool.query('SELECT * FROM Quizz WHERE id_creator=$1', [req.query.id_creator]);
+        res.json(result.rows);
+        res.status(200).end();
+    })
+
+    .get('/:id', async (req,res) => {
+        const result = pool.query('SELECT * FROM Quizz WHERE id=$1', [req.params.id]);
+        res.json(result.rows);
+        res.status(200).end();
+    })
+
     .get('/:id/questions',
         async (req, res) => {
             const result = await pool.query('SELECT * FROM questions WHERE id_quizz=$1', [req.params.id]);
             res.json(result.rows);
+            res.status(200).end();
         })
 
     .delete('/:id/delete',
--- back/routes/tags.js
+++ back/routes/tags.js
@@ -6,4 +6,29 @@ router
     .get('/', async (req,res)=> {
         const result = await pool.query('SELECT * FROM tags');
         res.json(result.rows);
+        res.status(200).end();
     })
+
+    .get('/:tagname', async (res,req) => {
+        const result = await pool.query('SELECT * FROM tags where tag=$1', [req.query.tagname]);
+
+        res.json(result.rows);
+        res.status(200).end();
+    })
+
+    .post('/', async (req,res) => {
+        const result = await pool.query(
+            'INSERT INTO tags (tag) VALUES ($1)', 
+            [req.body.tagname]);
+
+        res.status(201).end();
+    })
+
+    .delete('/:tagname', async (res,req) => {
+        const result = await pool.query(
+            'DELETE FROM tags WHERE tag=$1', 
+            [req.query.tagname]);
+
+        res.status(204).end();
+    });
+module.exports = router;
