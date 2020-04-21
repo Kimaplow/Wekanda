0e2f9f36543cbeea4681d23938f12f5e1f6a1a77
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-22 00:56:13 +0200

    :lipstick: Improving Header

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -4,17 +4,22 @@ import './css/header.css';
 export default function Header(){
 
     return(
-        <div className="header">
+        <div id="container">
 
-            <a href="#" className="logo">Wekanda</a>
+            <div>
+                <a href="#" id="title">
+                    <h1>Wekanda</h1>
+                </a>
+            </div>
+            
 
-            <div className="header-right">
+            <div id="header-right">
 
-                <input className={"search"} type="text" />
+                <input className="right-item" type="text" />
 
-                <a className={"filter"} href="#f"><img src={"/filtre.png"}/></a>
+                <a className="right-item" href="#f"><img src={"/filtre.png"}/></a>
 
-                <a className={"profil"} href="#p"><img src={"/icon_profil.png"}/></a>
+                <a className="right-item" href="#p"><img src={"/icon_profil.png"}/></a>
 
             </div>
         </div>
