870ccccb6efd7f2bf947efef9603fd4e8a1da2d5
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-21 18:08:50 +0200

    :sparkles: Added GET /:username

--- back/routes/users.js
+++ back/routes/users.js
@@ -3,17 +3,29 @@ const express = require('express');
 const router = express.Router();
 
 router
+    .get('/', async (req, res) => {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+        res.status(200).end();
+    })
 
-    .get('/',
-        async (req, res) => {
-            const result = await pool.query('SELECT * FROM users');
-            res.json(result.rows);
-        })
+    .get('/:username', async (req, res) => {
+        const result = await pool.query('SELECT * FROM users WHERE pseudo=$1', [req.params.username]);
+        res.json(result.rows);
+        res.status(200).end();
+    })
 
-    .get('/:id',
-        async (req, res) => {
-            const result = await pool.query('SELECT * FROM users WHERE id_user=$1', [req.params.id]);
-            res.json(result.rows);
-        });
+    
+
+    .post('/', async (req,res) => {
+        await pool.query('INSERT INTO users (pseudo,password) VALUES ($1,$2)',[req.body.username,req.body.password]);
+        res.status(201).end();
+    })
+
+    .delete('/:username', async (res,req) => {
+        const result = await pool.query('DELETE FROM users WHERE pseudo=$1', 
+                                        [req.params.username]);
+        res.status(204).end();
+    });
 
 module.exports = router;
