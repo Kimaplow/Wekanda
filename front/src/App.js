0eaddd9db5da665366da6464163e2742b0fa69f9
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-22 02:43:45 +0200

    :sparkles: Added blank Home Page and Router

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header';
-
+import Home from './components/Home';
 function App() {
   return (
-    <Header/>
+    <Router>
+        <Header/>
+        <div className={"container"}>
+                <Switch>
+                <Route exact={true} path='/'  component={Home}/>
+                <Route exact={true} path='/home' component={Home}/>
+                <Redirect from='*' to='/' />
+                </Switch>
+            </div>
+
+    </Router>
+    
   );
 }
 
