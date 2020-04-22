b9a92c87ec5ff808ced03e6e96f5ab132033df9d
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-22 15:56:40 +0200

    :art: Starting Profile Page

--- front/src/components/Profile.js
+++ front/src/components/Profile.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, UseState} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios'; 
 import './css/profile.css';
 
 export default function Profile(){
+    const {username} = useParams();
     return(
         <div id='profile-container'>
-            
+            <div i='top-profile'>
+                <img></img>
+                <h1>username</h1>
+            </div>
+
+            <div id='center-profile'>
+                <div id='user-quizzes'>
+
+                </div>
+
+                <div id='user-stats'>
+
+                </div>
+            </div>
         </div>
     );
 }
