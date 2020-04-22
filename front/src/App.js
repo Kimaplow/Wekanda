9cd36bec5987f3aa45d889061fb4496b2084f425
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-22 15:06:34 +0200

    :building_construction: Added blank components

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,10 @@ import './App.css';
 
 import Header from './components/Header';
 import Home from './components/Home';
+import Profile from './components/Profile';
+import Play from './components/Play';
+import Signin from './components/forms/Signin'
+import Signup from './components/forms/Signup'
 function App() {
   return (
 
@@ -16,6 +20,12 @@ function App() {
               <Switch>
                 <Route exact={true} path='/'  component={Home}/>
                 <Route exact={true} path='/home' component={Home}/>
+                <Route exact={true} path='/profile/:username' component={Profile}/>
+                <Route exact={true} path='/play/:id_quizz' component={Play}/>
+                <Route exact={true} path='/signin' component={Signin}/>
+                <Route exact={true} path='/signup' component={Signup}/>
+
+
                 <Redirect from='*' to='/' />
               </Switch>
         </div>
---
+++ front/src/components/Play.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/play.css';
+
+export default function Play(){
+    return(
+        <div id='play-container'>
+            
+        </div>
+    );
+}
