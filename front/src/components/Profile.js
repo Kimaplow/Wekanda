87ecaebb254fb057bc5536e56543a75362ec8c73
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-25 17:32:23 +0200

    WIP Profile

--- front/src/components/Profile.js
+++ front/src/components/Profile.js
@@ -10,21 +10,37 @@ export default function Profile() {
     const { id_user } = useParams();
 
     const [user, setUser] = useState({});
-    const [userquizzes, setUserQuizzes] = useState([]);
+    const [userQuizzes, setUserQuizzes] = useState([]);
     const [userScores, setUserScores] = useState([]);
 
+<<<<<<< HEAD
     async function fetchUser() {
+=======
+    console.log(`http://${config.server}/quizzes/fromuser/${id_user}`);
+
+
+    async function fetchUser(){
+>>>>>>> b9ce6c0... WIP Profile
         await axios.get(`http://${config.server}/users/${id_user}`)
             .then((res) => {
                 setUser(res.data[0]);
             });
     }
 
+<<<<<<< HEAD
     async function fetchUserQuizzes() {
         await axios.get(`http://${config.server}/quizzes/${id_user}/fromuser/`)
             .then((res) => {
                 setUserQuizzes(res.data);
             });
+=======
+    async function fetchUserQuizzes(){
+        await axios.get(`http://${config.server}/quizzes/${id_user}/fromuser`)
+                   .then((res)=>{
+                       console.log(res.data);
+                       setUserQuizzes(res.data);
+                   });
+>>>>>>> b9ce6c0... WIP Profile
     }
 
     async function fetchUserScores() {
@@ -37,8 +53,13 @@ export default function Profile() {
     useEffect(() => {
         fetchUser();
         fetchUserQuizzes();
+<<<<<<< HEAD
         fetchUserScores();
     }, [])
+=======
+        //fetchUserScores();
+    },[])
+>>>>>>> b9ce6c0... WIP Profile
 
     function userInfoJSX() {
         if (user !== undefined) {
@@ -55,6 +76,7 @@ export default function Profile() {
         }
     }
 
+<<<<<<< HEAD
     function userQuizzesJSX() {
         if (userquizzes !== undefined && userquizzes.length > 0) {
 
@@ -68,6 +90,26 @@ export default function Profile() {
                         })
                     }
                 </ul>
+=======
+    function userQuizzesJSX(){
+        if(userQuizzes !== undefined && userQuizzes.length > 0){
+            console.log('lala')
+            console.log(userQuizzes);
+            
+            
+            return(
+                <div id='user-quizzes' className='main-item'>
+                    <ul>
+                        {
+                        userQuizzes.map(quizz => {
+                            return(
+                                <li><QuizzCard quizz={quizz} width={500}/></li>
+                            )
+                        })
+                        }
+                    </ul>
+                </div>
+>>>>>>> b9ce6c0... WIP Profile
             );
         } else {
 
@@ -75,6 +117,7 @@ export default function Profile() {
     }
 
     function userStatsJSX() {
+<<<<<<< HEAD
         return (
             <div>
                 <h5>Nombre de quizz crées : </h5>
@@ -83,13 +126,31 @@ export default function Profile() {
                 <h5>Nombre de points marqués au total : </h5>
             </div>
         );
+=======
+        if(userScores !== undefined && userScores.length>0) {
+            return(
+                <div id='user-stats'>
+                        <ul>
+                        
+                        </ul> 
+                    </div>
+            );
+        }
+>>>>>>> b9ce6c0... WIP Profile
     }
 
     return (
         <div id='profile-container'>
 
+<<<<<<< HEAD
             <div id={"user-info"}>
             {userInfoJSX()}
+=======
+            <div id='center-profile'>
+                    
+                {userQuizzesJSX()}
+                {userStatsJSX()}
+>>>>>>> b9ce6c0... WIP Profile
             </div>
 
             
