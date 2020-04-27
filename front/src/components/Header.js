9b1f917c7fb53773bf285a8257e1ff90256ebd79
Galimede <mat.degand@outlook.fr>
2020-04-27 16:04:23 +0200

    :sparkles: Added dropdown for the future tag filter

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,34 +1,59 @@
 import React from 'react';
-import './css/materialize.css';
 import './css/header.css';
+import 'materialize-css';
+import { Dropdown, Icon } from 'react-materialize';
 
-export default function Header(){
+export default function Header() {
 
-    return(
+    return (
+        <div className="header">
+            <nav>
+                <div className="nav-wrapper grey darken-4">
 
-        <nav>
-            <div className="nav-wrapper grey darken-4">
 
-                
 
-                <a href="/home" className="brand-logo center">Wekanda</a>
+                    <a href="/home" className="brand-logo center">Wekanda</a>
 
-                <ul className="right hide-on-med-and-down">
-                    <li>
-                        <form>
-                            <div className="input-field">
-                                <input id="search" type="search" required />
+                    <ul className="right hide-on-med-and-down">
+                        <li>
+                            <form>
+                                <div className="input-field">
+                                    <input id="search" type="search" required />
                                     <label className="label-icon" htmlFor="search"><i
                                         className="material-icons">search</i></label>
                                     <i className="material-icons">close</i>
-                            </div>
-                        </form>
-                    </li>
-                    <li><a href="#"><i className="material-icons">tune</i></a></li>
-                    <li><a href="#"><i className="material-icons">person_outline</i></a></li>
-                </ul>
+                                </div>
+                            </form>
+                        </li>
+                        <li>
+                            <Dropdown
+                                id="dropdown-filter"
+                                options={{
+                                    alignment: 'right',
+                                    autoTrigger: true,
+                                    closeOnClick: true,
+                                    constrainWidth: true,
+                                    container: null,
+                                    coverTrigger: true,
+                                    hover: false,
+                                    inDuration: 150,
+                                    onCloseEnd: null,
+                                    onCloseStart: null,
+                                    onOpenEnd: null,
+                                    onOpenStart: null,
+                                    outDuration: 250
+                                }}
+                                trigger={<a node="button"><Icon>tune</Icon></a>}
+                            >
+                                <a href="#">select</a>
 
-            </div>
-        </nav>
+                            </Dropdown>
+                        </li>
+
+                        <li><a href="#"><i className="material-icons">person_outline</i></a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     );
 }
