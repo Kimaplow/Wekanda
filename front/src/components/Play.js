5cc02ba16a7802d71529358fc4f9645f707e7cc1
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-04-28 15:34:38 +0200

    :construction: WIP Making quizzes playable

--- front/src/components/Play.js
+++ front/src/components/Play.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import config from '../config';
@@ -13,9 +14,13 @@ export default function Play(){
     const [questions, setQuestions]  = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState({});
     const [currentAnswers, setCurrentAnswers] = useState([]);
-    const [currentidx, setCurrentidx] = useState([0]);
+    const [currentidx, setCurrentidx] = useState(0);
 
-    const [score, setScore] = useState([0]);
+    const [score, setScore] = useState(0);
+
+    const [answered, setAnswered] = useState(false)
+
+    let nextButton;
 
     async function fetchQuizz(){
         await axios.get(`http://${config.server}/quizzes/${id_quizz}`)
@@ -32,9 +37,9 @@ export default function Play(){
     }  
 
     async function fetchCurrentAnswers(){
-            await axios.get(`http://${config.server}/questions/${currentQuestion.id_question}/answers`)
+        await axios.get(`http://${config.server}/questions/${currentQuestion.id_question}/answers`)
                    .then(res => {
-                       
+                    //    console.log(res.data)
                        setCurrentAnswers(res.data);
                    });        
     }
@@ -42,12 +47,31 @@ export default function Play(){
     useEffect(() => {
         fetchQuizz();
         fetchQuestions();
+        nextButton = document.getElementById('next-button');
     },[])
     
     useEffect(()=> {
         setCurrentQuestion(questions[currentidx]);
-        console.log(currentQuestion)
     }, [currentidx]);
+
+    useEffect(()=> {
+        fetchCurrentAnswers();
+    }, [currentQuestion])
+
+    function handleAnswer(answer){
+        if (!answered) {
+            if(answer.correct){
+                setScore(parseInt(score)+10);
+            }else{
+                
+            }
+            setAnswered(true);
+            ReactDOM.findDOMNode(nextButton).style.color = 'blue';
+        }else{
+
+        }    
+        console.log('reponse');
+    }
 
     return(
 
@@ -56,21 +80,30 @@ export default function Play(){
                 <h2>{quizz ? quizz.title : "Quizz not found"}</h2>
             </div>
             <div id='question'>
-                {currentQuestion ? <h2 id=''>{currentQuestion.question}</h2> : ""}
+                {currentQuestion ? <h2>{currentQuestion.question}</h2> : ""}
             </div>
-
-            { currentAnswers ? 
-                (<div id='answers'>
-                    
-                </div>)
-                : "Answers not found"
-            }
-            <button onClick={() => {
+            <div id='score'>
+                <h2>Score : {score ? score : 0}</h2>
+            </div>
+            <div id='answers'>
+                { currentAnswers ? 
+                    currentAnswers.map((a, idx) => {
+                        return(
+                            <div className='answer' 
+                                 id={'answer'+idx} 
+                                 key={idx}
+                                 onClick={e => {handleAnswer(a)}}>
+                                    <h2>{a.answer}</h2>
+                            </div>
+                        );
+                    }) : "Answers not found"
+                }
+            </div>
+            
+            <button id='next-button' onClick={e => {
                 setCurrentidx(parseInt(currentidx)+1);
-                console.log(currentidx)
-            }}>
-                Next
-            </button>
+                setAnswered(false);
+            }}>Next</button>
         </div>
     );
 }
