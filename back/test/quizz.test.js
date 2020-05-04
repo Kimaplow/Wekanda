5ec84b8bf3e1a2e36a9719c2527e1f4a6b4a41fc
Galimede <mat.degand@outlook.fr>
2020-05-04 15:14:55 +0200

    :white_check_mark: Updating tests with id and tags (+fix timeout)

--- back/test/quizz.test.js
+++ back/test/quizz.test.js
@@ -14,8 +14,8 @@ describe('Quizzes', () => {
             .end((err,res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
-                done();
             });
+            done();
         });
     });
     describe('GET /quizzes/:id', () => {
@@ -25,8 +25,8 @@ describe('Quizzes', () => {
             .get("/quizzes/9999")
             .end((err,res) => {
                 res.should.have.status(404);
-                done();
             });
+            done();
         });
         it('should return 500 with an input who is not a number', (done) => {
             chai
@@ -34,8 +34,43 @@ describe('Quizzes', () => {
             .get("/quizzes/toto")
             .end((err,res) => {
                 res.should.have.status(500);
-                done();
             });
+            done();
+        });
+        it('should return 200 with an existring entry (/1)', (done) => {
+            chai.request(app)
+            .get("/quizzes/1")
+            .end((err,res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('id_creator');
+                res.body.should.have.property('id_quizz');
+                res.body.should.have.property('title');
+                res.body.should.have.property('path_file');
+                res.body.should.have.property('difficulty');
+            });
+            done();
+        });
+    });
+    describe('GEt /:id/withtags', () => {
+        it('should return 200 and all quizzes with their tags', (done) => {
+            chai
+            .request(app)
+            .get('/quizzes/withtags')
+            .end((err,response) => {
+                // console.log(res);
+                response.should.have.status(200);
+                response.body.forEach(res => {
+                    res.should.be.a('object');
+                    res.should.have.property('id_creator');
+                    res.should.have.property('id_quizz');
+                    res.should.have.property('title');
+                    res.should.have.property('path_file');
+                    res.should.have.property('difficulty');
+                    res.should.have.property('tags');
+                });
+            });
+            done();
         });
     });
 });
