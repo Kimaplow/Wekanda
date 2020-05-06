64abf312e32da431b27d3a3f603607e0610f1960
Galimede <mat.degand@outlook.fr>
2020-05-06 16:53:49 +0200

    :bug: fix for DELETE quizzes/:id/delete
    Displaying error and returning 404 if the quizz can't be deleted because it does not exist

--- back/routes/quizz.js
+++ back/routes/quizz.js
@@ -1,11 +1,9 @@
-
 const pool = require('../data/pg.js');
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 
-var storage = multer.diskStorage(
-    {
+var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         console.log(req.file);
         cb(null, './public/img');
@@ -16,7 +14,9 @@ var storage = multer.diskStorage(
     }
 });
 
-var upload = multer({ storage: storage });
+var upload = multer({
+    storage: storage
+});
 
 router
 
@@ -36,39 +36,62 @@ router
     .get('/withtags/:tag',
         async (req, res) => {
             const result = await pool.query(`select * from quizz 
-        left join tagquizz on quizz.id_quizz = tagquizz.id_quizz where tag=$1;`,[req.params.tag]);
-        if(result.rows.length === 0) {
-            return res.status(404).send({error:'Tag does not exist or has not been written correctly'});
-        }
+        left join tagquizz on quizz.id_quizz = tagquizz.id_quizz where tag=$1;`, [req.params.tag]);
+            if (result.rows.length === 0) {
+                return res.status(404).send({
+                    error: 'Tag does not exist or has not been written correctly'
+                });
+            }
             res.json(result.rows);
         })
     .get('/:id', async (req, res) => {
-        if(isNaN(+req.params.id)) {
-          return res.status(500).send({error:'Input given is not a number'});
+        if (isNaN(+req.params.id)) {
+            return res.status(500).send({
+                error: 'Input given is not a number'
+            });
         }
         const result = await pool.query('SELECT * FROM quizz WHERE id_quizz=$1', [req.params.id]);
-        if(result.rows.length === 0) {
-            return res.status(404).send({error:'Quizz not found'});
-        } 
+        if (result.rows.length === 0) {
+            return res.status(404).send({
+                error: 'Quizz not found'
+            });
+        }
         res.json(result.rows[0]);
     })
     .get('/:id/fromuser', async (req, res) => {
         const result = await pool.query('SELECT * FROM quizz WHERE id_creator=$1', [req.params.id]);
-        if(result.rows.length === 0) {
-            return res.status(404).send({error:'Quizz not found for this user or user does not exist'});
+        if (result.rows.length === 0) {
+            return res.status(404).send({
+                error: 'Quizz not found for this user or user does not exist'
+            });
         }
         res.json(result.rows);
     })
     .get('/:id/questions',
         async (req, res) => {
             const result = await pool.query('SELECT * FROM questions WHERE id_quizz=$1', [req.params.id]);
+            if (result.rows.length === 0) {
+                return res.status(404)
+                    .send({
+                        error: 'questions not found for this quizz or quizz does not exist'
+                    });
+            }
             res.json(result.rows);
         })
 
     .delete('/:id/delete',
         async (req, res) => {
-            await pool.query('DELETE FROM quizz WHERE id_quizz = $1', [req.params.id]);
-            res.status(201).end();
+            const result = await pool.query('DELETE FROM quizz WHERE id_quizz = $1', [req.params.id], (err, res) => {
+                if (err) return;
+                else return res;
+            });
+            if (result === undefined) {
+                return res.status(404)
+                    .send({
+                        error: 'Quizz can\'t be deleted because it doesn\'t exist'
+                    });
+            }
+            res.status(204).send(res);
         })
 
     .patch('/:id',
@@ -78,7 +101,7 @@ router
                 await pool.query('UPDATE quizz SET title = $1 WHERE id_quizz=$2', [req.body.title, req.params.id]);
             }
 
-            if (req.body.path_file !== '') {              
+            if (req.body.path_file !== '') {
                 await pool.query('UPDATE quizz SET path_file=$1 WHERE id_quizz=$2', [req.body.path_file, req.params.id]);
             }
 
