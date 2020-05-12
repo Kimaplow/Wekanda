678d554921b181223535369aed9b62dc9da9f4af
francois_lannoy <francois_lannoy@ens.univ-artois.fr>
2020-05-12 16:23:30 +0200

    :art: updating css

--- front/src/components/QuizzCard.js
+++ front/src/components/QuizzCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import config from '../config';
+import './css/quizzcard.css';
 
 
 
@@ -14,18 +15,18 @@ export default function QuizzCard(props) {
 
         <div className="col s12 m7" style = {{maxWidth: `${props.width}px`,height: `${height}px`,
          marginBottom: '5%', marginLeft: '1%'}}>
-            <h4 className="header" style={{marginTop: '10%'}}>{props.quizz.title}</h4>
+            <h4 className="header">{props.quizz.title}</h4>
             <div className="card horizontal" >
                 <div className="card-image"  >
                     <img style = {{height: `${height}px`, width: '150px'}} src={`http://${config.server}/img/${props.quizz.path_file}`} alt={`${props.quizz.path_file}`}/>
                 </div>
                 <div className="card-stacked">
-                    <div className="card-content" style = {{color : '#3D3D3E'}}>
-                        <p>{props.quizz.description}</p>
+                    <div className="card-content">
+                        <p id='desc'>{props.quizz.description}</p>
                     </div>
                     <div className="card-action">
                         <a href={`/quizz/${props.quizz.id_quizz}/play`}>Jouer</a>
-                        <a href={`/quizz/${props.quizz.id_quizz}/edit`} style = {{marginLeft : '150px'}}>Modifier</a>
+                        <a id='modify-quizz-button' href={`/quizz/${props.quizz.id_quizz}/edit`}>Modifier</a>
                     </div>
                 </div>
             </div>
