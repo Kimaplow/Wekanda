91c41963abfe5c33360f930090f615d3a41bd208
Mathieu Degand <32012475+Galimede@users.noreply.github.com>
2020-05-13 13:59:01 +0200

    :pencil2: Fix typo config back

--- back/data/config.js
+++ back/data/config.js
@@ -2,7 +2,7 @@ const config = {
     pgUser: process.env.PGUSER,
     pgHost: process.env.PGHOST,
     pgDatabase: process.env.PGDATABASE,
-    pgPassword: process.env.PGPASSOWRD,
+    pgPassword: process.env.PGPASSWORD,
     pgPort: process.env.PGPORT
 };
 
