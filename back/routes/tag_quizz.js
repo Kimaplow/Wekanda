8d002c5c30fb92541dfb0ea2c5b2e649f4ec5094
Galimede <mat.degand@outlook.fr>
2020-05-19 16:19:56 +0200

    :sparkles: add POST for tagQuizz route

--- back/routes/tag_quizz.js
+++ back/routes/tag_quizz.js
@@ -8,6 +8,11 @@ router
         async (req, res) => {
             const result = await pool.query('SELECT * FROM tagQuizz');
             res.json(result.rows);
-        });
+        })
+    .post("/", 
+    async (req,res) => {
+        const result = await pool.query("INSERT INTO tagquizz(id_quizz, tag) values($1,$2)", [req.body.id_quizz,req.body.tag]);
+        res.status(201).end();
+    });
 
 module.exports = router;
