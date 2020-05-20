22b089d8b548bb358c639a3ade10d72cfc79ca48
Galimede <mat.degand@outlook.fr>
2020-05-20 15:29:49 +0200

    :bug: fix http code and api problems for answers

--- back/routes/answers.js
+++ back/routes/answers.js
@@ -1,44 +1,92 @@
 const pool = require('../data/pg.js');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        console.log(req.file);
+        cb(null, './public/img');
+    },
+    filename: function (req, file, cb) {
+        console.log(req.body.path_file);
+        cb(null, req.body.path_file);
+    }
+});
+
+var upload = multer({
+    storage: storage
+});
 
 router
-    .get('/', async (req,res) => {
+    .get('/', async (req, res) => {
         const result = await pool.query('SELECT * FROM answers');
         res.json(result.rows);
     })
 
     .post('/',
-        async (req, res) => { console.log("files", req.files); console.log("body", [req.body]);
+        upload.single('file'), async (req, res) => {
             await pool.query('INSERT INTO answers(id_question, answer, correct, path_file) VALUES($1, $2, $3, $4)',
-                [req.body.id_question, req.body.answer, req.body.correct, req.files.file.name]);
-            const moved = moveToPath(req.files.file);
-            if(moved) res.status(201).end();
-            else res.status(500).send('file not allowed or error during the upload');
-    })
+                [req.body.id_question, req.body.answer, req.body.correct, req.body.path_file]);
+            res.status(201).end();
+        })
 
-    .get('/:id', async (req,res) => {
-        const result = await pool.query('SELECT * FROM answers WHERE id_answers=$1', [req.params.id]);
-        res.json(result.rows);
+    .get('/:id', async (req, res) => {
+        const result = await pool.query('SELECT * FROM answers WHERE id_answer=$1', [req.params.id]);
+        if (result.rowCount === 0) {
+            res.status(404).send({
+                error: "Answer not found for this id"
+            });
+        }
+        res.json(result.rows[0]);
     })
-
     .patch('/:id',
         async (req, res) => {
 
-            if (req.body.answer !== null && req.body.answer !== '') {
-                await pool.query('UPDATE answers SET answer=$1 WHERE id_answer=$2', [req.body.answer, req.params.id]);
+            let result = undefined;
+          
+
+            if (typeof req.body.answer !== "undefined") {
+                if (typeof req.body.answer !== "string" ) { // check for syntax error
+                    return res.status(403).send("Wrong type for the answer");
+                } 
+                if(req.body.answer === "")  // check for empty string
+                return res.status(400).send({error:"Syntax error"});
+                result = await pool.query('UPDATE answers SET answer=$1 WHERE id_answer=$2', [req.body.answer, req.params.id]);
+                if (result.rowCount === 0) {
+                    return res.status(404).send({
+                        error: "Answer not found for this id"
+                    });
+                }
             }
 
-            if (req.body.correct !== null && req.body.correct !== '') { 
-                await pool.query('UPDATE answers SET correct=$1 WHERE id_answer=$2', [req.body.correct, req.params.id]);
+            if (typeof req.body.correct !== "undefined") {
+                if (typeof req.body.correct !== "boolean")
+                    return res.status(403).send("Wrong type for the correct option"); 
+                result = await pool.query('UPDATE answers SET correct=$1 WHERE id_answer=$2', [req.body.correct, req.params.id]);
+                if (result.rowCount === 0) {
+                    return res.status(404).send({
+                        error: "Answer not found for this id"
+                    });
+                }
             }
-
-            res.status(204).end();
-
+            
+            if(typeof result === "undefined") {
+                return res.status(400).send({
+                    error: "Wrong input or nothing was provided"
+                });
+            } 
+            
+            return res.status(204).end();
         })
 
-    .delete('/:id', async (req,res)=>{
+    .delete('/:id', async (req, res) => {
         const result = await pool.query('DELETE FROM answers WHERE id_answer=$1', [req.params.id]);
+        if (result.rowCount === 0) {
+            res.status(404).send({
+                error: "Answer not found for this id"
+            });
+        }
         res.status(204).end();
     });
 
