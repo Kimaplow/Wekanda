dafe1b17e295a34e1d194206572393c75e517ef5
Galimede <mat.degand@outlook.fr>
2020-05-20 15:30:42 +0200

    :bug: fix http code and api problems for questions

--- back/routes/questions.js
+++ back/routes/questions.js
@@ -24,20 +24,23 @@ router
         async (req, res) => {
             const result = await pool.query('SELECT * FROM questions');
             res.json(result.rows);
-            res.status(200).end();
         })
 
     .get('/:id_question',
         async (req, res) => {
             const result = await pool.query('SELECT * FROM questions WHERE id_question=$1', [req.params.id_question]);
-            res.json(result.rows);
-            res.status(200).end();
+            if(result.rowCount === 0) {
+                return res.status(404).send({error: "Question not found"});
+            }
+            res.json(result.rows[0]);
         })
 
     .get('/:id_question/answers', async (req, res) => {
         const result = await pool.query('SELECT * FROM answers WHERE id_question=$1', [req.params.id_question]);
+        if(result.rowCount === 0) {
+            return res.status(404).send({error: "Answers for this question not found"});
+        }
         res.json(result.rows);
-        res.status(200).end();
     })
 
     .post('/',
@@ -50,20 +53,34 @@ router
     .delete('/:id',
         async (req, res) => {
             const result = await pool.query('DELETE FROM questions WHERE id_question=$1', [req.params.id]);
-            res.status(204);
+            if(result.rowCount === 0) {
+                return res.status(404).send({error: "Question not found and therefore cant be deleted"});
+            }
+            res.status(204).end();
         })
 
     .patch('/:id',
     upload.single('file'), async (req, res) => {
 
-        if (req.params.question) {
-            await pool.query('UPDATE questions SET question = $1 WHERE id_question=$2', [req.body.question, req.params.id]);
+        let result = undefined;
+
+        if (typeof req.body.question !== "undefined") {
+            if(typeof req.body.question !== "string")
+            return res.status(403).send("Wrong type for answer");
+            result = await pool.query('UPDATE questions SET question = $1 WHERE id_question=$2', [req.body.question, req.params.id]);
         }
 
-        if (req.params.path_file && req.file) {
-            await pool.query('UPDATE questions SET path_file = $1 WHERE id_question=$2', [req.body.path_file, req.params.id]);
+        if (typeof req.body.path_file !== "undefined" && req.file) {
+            if(typeof req.body.path_file !== "string")
+            return res.status(403).send("Wrong type for path_file");
+            result = await pool.query('UPDATE questions SET path_file = $1 WHERE id_question=$2', [req.body.path_file, req.params.id]);
         }
 
+        if(typeof result === "undefined") {
+            return res.status(400).send({
+                error: "Wrong input or nothing was provided"
+            });
+        }
         res.status(204).end();
     });
 
