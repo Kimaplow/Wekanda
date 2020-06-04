dfea61dbf9cd4bf4314260bbfcb19e4b8c962b44
Mathieu Degand <mat.degand@outlook.fr>
2020-06-04 15:21:22 +0200

    :construction: Added signin component

--- front/src/components/Signin.js
+++ front/src/components/Signin.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './css/signin.css';
+import { TextInput, Button } from "react-materialize";
+import {Link} from "react-router-dom";
+async function onSubmit(e) {
 
-export default function Signin(){
-    return(
-        <div id='signin-container'>
-            
+}
+
+export default function Signin() {
+    return (
+        <div>
+            <h1>Se connecter</h1>
+            <div id='signin-container'>
+            <div id="form-container">
+                    <form onSubmit={e => onSubmit(e)}>
+                        <TextInput
+                            id="email"
+                            label="Email"
+                            email
+                            validate
+                        />
+                        <TextInput
+                            id="password"
+                            label="Password"
+                            password
+                        />
+                        <Button>Se connecter</Button>
+                    </form>
+                    <p>Pas de compte ? <Link to="/signup">S'inscrire</Link></p>
+                </div>
+            </div>
         </div>
     );
 }
