5ce597baf20b4ddaa418b1159cdc994d6ecc8226
christophe_guide <christophe_guide@ens.univ-artois.fr>
2020-06-09 15:19:03 +0200

    id unique pour les images côté serveur

--- back/tools/multer_config.js
+++ back/tools/multer_config.js
@@ -1,13 +1,19 @@
 const multer = require('multer');
 
+function uniqueName(filename) {
+    let index = filename.indexOf(".");
+    let rootFilename = filename.substr(0, index);
+    return rootFilename + Date.now() + filename.substr(index);
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         console.log(req.file);
         cb(null, './public/img');
     },
     filename: function (req, file, cb) {
-        console.log(req.body.path_file);
-        cb(null, req.body.path_file);
+        let chemin = uniqueName(req.body.path_file);
+        cb(null, chemin);
     }
 });
 
