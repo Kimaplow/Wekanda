dcee8b290a575d61aade17768ce912e3ea66e0df
christophe_guide <christophe_guide@ens.univ-artois.fr>
2020-06-15 15:24:12 +0200

    Le tag ne s'affiche uniquement si un quizz le possède

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -26,7 +26,7 @@ export default function Home() {
             .catch(err => console.log(err));
         // Eventually we bring themp up together to have for a tag all the quizzes associated to it
         let tmp = [];
-        
+
         tags.map((t) => {
             quizzes.forEach(q => {
                 if (q.tags && q.tags.includes(t.tag)) tmp.push(q);
@@ -42,23 +42,28 @@ export default function Home() {
     }, []);
 
     function renderEachQuizz(quizzes) {
-        return quizzes.map(function (q, index){
+        return quizzes.map(function (q, index) {
             return (
-            <li className={'card_quizz'} key={index}>
-                <QuizzCard width={500} key={index} quizz={q} />
-            </li>)
+                <li className={'card_quizz'} key={index}>
+                    <QuizzCard width={500} key={index} quizz={q} />
+                </li>)
         });
     }
 
 
     return (
         <div id='container'>
-            {tagsQuizzes ? tagsQuizzes.map((tq, index) =>
-                <div className={"tq_tag"} key={index}>
-                    <h1>{tq.tag}</h1>
-                    <ul className='cards'>{renderEachQuizz(tq.quizzes)}</ul>
-                </div>)
-            : 'no quizz found'}
+            {tagsQuizzes ? tagsQuizzes.map((tq, index) => {
+                if (tq.quizzes.length > 0) {
+                    return (
+                    <div className={"tq_tag"} key={index}>
+                        <h1>{tq.tag}</h1>
+                        <ul className='cards'>{renderEachQuizz(tq.quizzes)}</ul>
+                    </div>
+                    )
+                }
+            })
+            : undefined}
         </div>
     );
 }
