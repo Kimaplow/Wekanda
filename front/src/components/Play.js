576d1eaf04e971d8fa5f98e4e61a43fada9993aa
christophe_guide <christophe_guide@ens.univ-artois.fr>
2020-06-18 12:18:49 +0200

    Améliore affichage avertissement dans Play.js

--- front/src/components/Play.js
+++ front/src/components/Play.js
@@ -13,7 +13,7 @@ import sleep from '../tools/sleep';
 export default function Play() {
     const { id_quizz } = useParams();
     const history = useHistory();
-    
+
     const [quizz, setQuizz] = useState({});
     const [questions, setQuestions] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(undefined);
@@ -29,19 +29,19 @@ export default function Play() {
     /* flag to make it impossible to answer the same Q several times */
     const [answered, setAnswered] = useState(false);
 
-    const [alert,setAlert] = useState();
+    const [alert, setAlert] = useState();
     const [cookies, setCookie, removeCookie] = useCookies(['login']);
     const [user, setUser] = useState(undefined);
 
     /** We first verify that the user is connected in order to play a quizz */
     async function verifyToken() {
         axios.defaults.headers.common['Authorization'] = (cookies.login ? 'Bearer ' + cookies.login.token : null);
-        await axios.get(`http://${config.server}/users/verify_token`, { responseType: 'text'})
-        .catch(async err => {
-            setAlert(<CardPanel className="orange"><Icon tiny>error</Icon>Merci de vous connecter</CardPanel>);
-                await sleep(800);
+        await axios.get(`http://${config.server}/users/verify_token`, { responseType: 'text' })
+            .catch(async err => {
+                setAlert(<CardPanel className="orange"><Icon tiny>error</Icon>Merci de vous connecter</CardPanel>);
+                await sleep(2500);
                 history.push('/signin');
-        })
+            })
     }
 
     async function fetchUser() {
@@ -54,8 +54,8 @@ export default function Play() {
                 .catch(err => false);
         }
     }
-    
-    async function getScore(){
+
+    async function getScore() {
         await axios.get(`http://${config.server}/scores/${user.id_user}/user/${id_quizz}/quizz`)
             .then(res => {
                 setScoreDB(res.data);
@@ -89,10 +89,10 @@ export default function Play() {
             });
     }
 
-    async function postScore(){
+    async function postScore() {
         let req = {
-            id_quizz : quizz.id_quizz,
-            score : score
+            id_quizz: quizz.id_quizz,
+            score: score
         }
         await axios.post(`http://${config.server}/scores`, req);
         document.querySelector('#postScore').innerHTML = 'Score envoyé !'
@@ -108,7 +108,7 @@ export default function Play() {
     }, [])
 
     useEffect(() => {
-        if(user){
+        if (user) {
             getScore();
         }
     }, [user])
@@ -121,17 +121,17 @@ export default function Play() {
         if (currentQuestion) fetchCurrentAnswers();
     }, [currentQuestion]);
 
-    
+
     useInterval(() => {
-        if(second > 1){
-            if(!chronoTermine){
+        if (second > 1) {
+            if (!chronoTermine) {
                 setSecond(second - 1)
             }
         }
-        else{
-            if(!chronoTermine){
+        else {
+            if (!chronoTermine) {
                 setSecond('Terminé');
-                let a = {'correct' : false};
+                let a = { 'correct': false };
                 handleAnswer(a);
             }
         }
@@ -157,14 +157,14 @@ export default function Play() {
         }, [delay]);
     }
 
-    function handleAnswer(answer){
+    function handleAnswer(answer) {
         setChronoTermine(true);
         if (!answered) {
             /* Checking if the user has answered correctly */
             if (answer.correct) {
                 setScore(parseInt(score) + 10);
                 document.querySelector('#feedback').innerHTML = 'Bonne réponse !';
-                
+
             } else {
                 setScore(parseInt(score) - quizz.difficulty * 2)
                 document.querySelector('#feedback').innerHTML = 'Mauvaise réponse !';
@@ -178,10 +178,10 @@ export default function Play() {
             setAnswered(true);
 
             /* Checking if the quizz is over */
-            if (currentidx < questions.length - 1){
+            if (currentidx < questions.length - 1) {
                 document.querySelector('#next-button').style.visibility = 'visible';
             }
-            else{
+            else {
                 document.querySelector('#finish-button').style.visibility = 'visible';
             }
         }
@@ -229,74 +229,81 @@ export default function Play() {
 
 
     const trigger = <Button id='finish-button'>Terminer</Button>
-  
-    return (
 
-        <div id='play-container'>
+    if (alert) {
+        return (
             <span id="alert-box">
                 {alert}
             </span>
-            <div id='quizz-title'>
-                {quizz && quizz === 'not found' ? <Redirect to='/' /> : ''}
-                <h2>{quizz ? quizz.title : "Quizz not found"}</h2>
+        )
+    }
+    else {
+        return (
+            <div id='play-container'>
+
+                <div id='quizz-title'>
+                    {quizz && quizz === 'not found' ? <Redirect to='/' /> : ''}
+                    <h2>{quizz ? quizz.title : "Quizz not found"}</h2>
+                </div>
+
+                {currentQuestion ? displayQuestion(currentQuestion) : ''}
+
+                <div id='score'>
+                    <h2>Score : {score}</h2>
+                </div>
+
+                <div id='chrono'>
+                    Temps restant : {second}
+                </div>
+
+                <div id='answers'>
+                    {currentAnswers ?
+                        currentAnswers.map((a, idx) => {
+                            if (a.path_file.split('.')[1] !== 'jpg') {
+                                return (
+                                    <CardPanel className="answer" key={idx} id={'answer' + idx} onClick={e => { handleAnswer(a) }}>
+                                        <span className="white-text">
+                                            {a.answer}
+                                        </span>
+                                        {a.correct ? <Icon>check</Icon> : <Icon>clear</Icon>}
+                                    </CardPanel>
+                                );
+                            } else {
+                                return (
+                                    <CardPanel style={{
+                                        backgroundImage: 'url(' + `http://${config.server}/img/${a.path_file}` + ')'
+                                    }}
+                                        className="answer" key={idx} id={'answer' + idx} onClick={e => { handleAnswer(a) }}
+                                    >
+                                        {a.correct ? <Icon>check</Icon> : <Icon>clear</Icon>}
+                                    </CardPanel>
+                                );
+                            }
+                        }) : "Answers not found"
+                    }
+                </div>
+
+                <div>
+                    <p id='feedback'></p>
+                </div>
+
+                <div>
+                    <Button id='next-button' onClick={e => { handleNext() }}>Next</Button>
+                </div>
+
+                <div>
+                    <Modal header={quizz ? quizz.title : undefined} trigger={trigger}
+                        actions={[
+                            <Button id="postScore" node="button" onClick={e => { postScore() }}>Envoyer le score</Button>,
+                            <Button flat modal="close" node="button"><Icon className="close-modal">close</Icon></Button>
+                        ]}
+                    >
+                        <p>Votre meilleur score : {scoreDB ? scoreDB.score : 'Vous n\'avez pas encore de score pour ce quizz'}</p>
+                        <p>Votre score actuel : {score ? score : undefined}</p>
+                    </Modal>
+                </div>
+
             </div>
-
-            {currentQuestion ? displayQuestion(currentQuestion) : ''}
-
-            <div id='score'>
-                <h2>Score : {score}</h2>
-            </div>
-
-            <div id='chrono'>
-                Temps restant : {second}
-            </div>
-
-            <div id='answers'>
-                {currentAnswers ?
-                    currentAnswers.map((a, idx) => {
-                        if (a.path_file.split('.')[1] !== 'jpg') {
-                            return (
-                                <CardPanel className="answer" key={idx} id={'answer'+idx} onClick={e=>{handleAnswer(a)}}>
-                                    <span className="white-text">
-                                        {a.answer}
-                                    </span>
-                                    {a.correct ? <Icon>check</Icon> : <Icon>clear</Icon>}
-                                </CardPanel>
-                            );
-                        } else {
-                            return (
-                                <CardPanel style={{
-                                    backgroundImage: 'url(' + `http://${config.server}/img/${a.path_file}` + ')'}}
-                                    className="answer" key={idx} id={'answer'+idx} onClick={e=>{handleAnswer(a)}}
-                                >
-                                    {a.correct ? <Icon>check</Icon> : <Icon>clear</Icon>}
-                                </CardPanel>
-                            );
-                        }
-                    }) : "Answers not found"
-                }
-            </div>
-
-            <div>
-                <p id='feedback'></p>
-            </div>
-
-            <div>
-                <Button id='next-button' onClick={e => { handleNext() }}>Next</Button>
-            </div>
-
-            <div>
-                <Modal header={quizz ? quizz.title : undefined} trigger={trigger} 
-                    actions={[
-                        <Button id="postScore" node="button" onClick={e => {postScore()} }>Envoyer le score</Button>,
-                        <Button flat modal="close" node="button"><Icon className="close-modal">close</Icon></Button>                  
-                    ]}
-                >
-                    <p>Votre meilleur score : {scoreDB ? scoreDB.score : 'Vous n\'avez pas encore de score pour ce quizz'}</p>
-                    <p>Votre score actuel : {score ? score : undefined}</p>
-                </Modal>
-            </div>
-
-        </div>
-    );
+        );
+    }
 }
